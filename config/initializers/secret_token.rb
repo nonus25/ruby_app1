ba09357bea411b06df7ebde408b2159b64891274
nonus25 <nonus25@vaio-vgn-z21mn.(none)>
2011-09-19 04:36:24 +0100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = 'd85069545c99cc180c67a9eb4807b97ef3a52277db0111df8f8069f716647d715d5c9187bde22f76ea248efb51e89b66a44c7ea3d3895315f8ee56657a7e7e2d'
